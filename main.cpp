c20b9644c3e57b062afc612b85aae6ff4e38aeee
Lii02 <lukeinlow@gmail.com>
2023-02-16 19:39:41 -0500

    Pipes, sprite updates, math classes

--- main.cpp
+++ main.cpp
@@ -9,86 +9,176 @@
 #define STB_TRUETYPE_IMPLEMENTATION
 #include "stb_truetype.h"
 
+typedef std::chrono::high_resolution_clock::time_point TimePoint;
+#define UNIT_X 64
+#define UNIT_Y 64
+
 namespace {
-    SDL_Renderer* renderer;
+	SDL_Renderer* renderer;
 }
 
 struct Vector2 {
-    float x;
-    float y;
+	float x;
+	float y;
+
+	Vector2() {
+		this->x = 0;
+		this->y = 0;
+	}
+
+	Vector2(float v) {
+		this->x = v;
+		this->y = v;
+	}
+
+	Vector2(float x, float y) {
+		this->x = x;
+		this->y = y;
+	}
+
+	Vector2 operator-(Vector2 right) {
+		return Vector2(x - right.x, y - right.y);
+	}
+	
+	Vector2 operator+(Vector2 right) {
+		return Vector2(x + right.x, y + right.y);
+	}
+
+	Vector2 operator/(Vector2 right) {
+		return Vector2(x / right.x, y / right.y);
+	}
 };
 
-class Rectangle {
-public:
-    Vector2 position, size;
+struct Rectangle {
+	Vector2 position, size;
 
-    Rectangle() {
-        position = { 0, 0 };
-        size = { 1, 1 };
-    }
+	Rectangle() {
+		position = { 0, 0 };
+		size = { 1, 1 };
+	}
 
-    Rectangle(Vector2 position, Vector2 size) {
-        this->position = position;
-        this->size = size;
-    }
+	Rectangle(Vector2 position, Vector2 size) {
+		this->position = position;
+		this->size = size;
+	}
 };
 
 class Sprite {
 private:
-    SDL_Texture* texture;
+	SDL_Texture* texture;
 public:
-    Rectangle rectangle;
-    float angle;
+	Rectangle rectangle;
+	float angle;
 
-    Sprite(std::string filename, Rectangle rectangle, float angle) {
-        this->rectangle = rectangle;
-        this->angle = angle;
-        SDL_Surface* surface = IMG_Load(filename.c_str());
-        texture = SDL_CreateTextureFromSurface(renderer, surface);
-        SDL_FreeSurface(surface);
-        SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND);
-    }
-    
-    ~Sprite() {
-        SDL_DestroyTexture(texture);
-    }
+	Sprite(std::string filename, Rectangle rectangle, float angle) {
+		this->rectangle = rectangle;
+		this->angle = angle;
+		SDL_Surface* surface = IMG_Load(filename.c_str());
+		texture = SDL_CreateTextureFromSurface(renderer, surface);
+		SDL_FreeSurface(surface);
+		SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_BLEND);
+	}
 
-    void Draw() {
-        SDL_Rect r = { rectangle.position.x, rectangle.position.y, rectangle.size.x, rectangle.size.y };
-        SDL_RenderCopyEx(renderer, texture, NULL, &r, angle, NULL, SDL_FLIP_NONE);
-    }
+	~Sprite() {
+		SDL_DestroyTexture(texture);
+	}
+
+	void Draw() {
+		SDL_Rect r = { UNIT_X * rectangle.position.x, UNIT_Y * rectangle.position.y, UNIT_X * rectangle.size.x, UNIT_Y * rectangle.size.y };
+		SDL_RenderCopyEx(renderer, texture, NULL, &r, angle, NULL, SDL_FLIP_NONE);
+	}
+};
+
+bool IsColliding(Rectangle left, Rectangle right) {
+	Vector2 leftMin = left.position - left.size / 2.0f;
+	Vector2 leftMax = left.position + left.size / 2.0f;
+	Vector2 rightMin = right.position - right.size / 2.0f;
+	Vector2 rightMax = right.position + right.size / 2.0f;
+	if (leftMin.x < rightMax.x && leftMax.x > rightMin.x &&
+		leftMin.y < rightMax.y && leftMax.y > rightMin.y)
+		return true;
+	else
+		return false;
+}
+
+#define PIPE_HEIGHT 11
+
+class Pipe {
+private:
+	int entrance;
+	float x;
+	Sprite* spr;
+public:
+	Pipe() {
+		spr = new Sprite("pipe.png", Rectangle(), 0.0f);
+		entrance = rand() % PIPE_HEIGHT;
+	}
+
+	~Pipe() {
+		delete spr;
+	}
+
+	void Draw() {
+		for (int i = 0; i < PIPE_HEIGHT; i++) {
+			if (i == entrance || i == entrance + 1)
+				continue;
+			spr->rectangle.position = Vector2(x, i);
+			spr->Draw();
+		}
+	}
 };
 
 int main() {
-    SDL_Init(SDL_INIT_EVERYTHING);
-    IMG_Init(IMG_INIT_PNG);
-    SDL_Window* window = SDL_CreateWindow("Flappy Bird Clone", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 800, SDL_WINDOW_SHOWN);
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    bool running = true;
+	SDL_Init(SDL_INIT_EVERYTHING);
+	IMG_Init(IMG_INIT_PNG);
+	srand(time(NULL));
+	SDL_Window* window = SDL_CreateWindow("Flappy Bird Clone", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 832, 832, SDL_WINDOW_SHOWN);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	bool running = true;
 
-    Sprite* sprite = new Sprite("ship.png", Rectangle({ 0.0f, 0.0f }, { 128, 64 }), 0.0f);
+	const float playerStartY = 5.75f;
+	const float horizontalSpeed = 50.0f;
+	Sprite* sprite = new Sprite("ship.png", Rectangle({ 0.0f, playerStartY }, { 2, 1 }), 0.0f);
+	Sprite* floor = new Sprite("floor.png", Rectangle(), 0);
 
-    while (running) {
-        SDL_Event ev;
-        while (SDL_PollEvent(&ev)) {
-            switch (ev.type) {
-            case SDL_QUIT:
-                running = false;
-                break;
-            }
-        }
-        SDL_RenderClear(renderer);
-        SDL_SetRenderDrawColor(renderer, 50, 50, 50, 255);
+	TimePoint begin, end;
+	double delta = 0.0;
 
-        sprite->Draw();
+	while (running) {
+		begin = std::chrono::high_resolution_clock::now();
+		float dt = static_cast<float>(delta / 1000);
 
-        SDL_RenderPresent(renderer);
-    }
+		SDL_Event ev;
+		while (SDL_PollEvent(&ev)) {
+			switch (ev.type) {
+			case SDL_QUIT:
+				running = false;
+				break;
+			}
+		}
+		SDL_RenderClear(renderer);
+		SDL_SetRenderDrawColor(renderer, 50, 50, 50, 255);
 
-    delete sprite;
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    IMG_Quit();
-    SDL_Quit();
-    return 0;
+		for (int x = 0; x < 13; x++) {
+			for (int y = PIPE_HEIGHT; y < 15; y++) {
+				floor->rectangle.position = { (float)x, (float)y };
+				floor->Draw();
+			}
+		}
+
+		sprite->Draw();
+
+		SDL_RenderPresent(renderer);
+		end = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double, std::milli> deltaDuration = end - begin;
+		delta = deltaDuration.count();
+	}
+
+	delete floor;
+	delete sprite;
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	IMG_Quit();
+	SDL_Quit();
+	return 0;
 }
